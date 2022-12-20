fa3fe832a26d26c4e460fd7b4607b3da01e3a72b
KagontleBooysen <k.booysen@alustudent.com>
2022-12-20 09:44:56 -0800

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-""" Creates an empty class called Square
-"""
+"""Class Square"""
 
 
 class Square:
-    """ Empty class with size private attribute
+
+    """Private instance attribute: size
+    size must be an integer
+    The size is greater than or equal to 0
     """
+
     def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+
+        """that returns the current square area
         """
-                Instantiation with size
-        Args:
-            size: size of the square
-        """
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+
+        return (self.__size ** 2)
